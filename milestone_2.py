6afe40fd92ca5005f0dd0aa54645b8d088e0c423
migs9327 <miguel.nava1997@hotmail.co.uk>
2023-11-01 10:35:54 +0000

    Create milestone_2.py

---
+++ milestone_2.py
@@ -0,0 +1,15 @@
+import random
+word_list = ['banana', 'grape', 'strawberry', 'raspberry', 'orange']
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input('Enter a single letter: ')
+
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess!')
+else:
+    print("Oops! That is not a valid input.")
+
+
